5cfaeea4b7b9cfc40062c8a14db3e647779df383
caiweida <2332268008@qq.com>
2020-09-07 16:46:41 +0800

    1.加入旅游服务页面

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App'
+import lyfw from 'common/Lyfw.js' //旅游服务
 
 Vue.config.productionTip = false
 
@@ -8,4 +9,15 @@ App.mpType = 'app'
 const app = new Vue({
     ...App
 })
+
+//模拟异步请求数据 - 旅游服务
+const Lyfw = type=>{
+	return new Promise(resolve=>{
+		setTimeout(()=>{
+			resolve(lyfw[type]);
+		}, 500)
+	})
+}
+
+Vue.prototype.$api = {lyfw};
 app.$mount()
